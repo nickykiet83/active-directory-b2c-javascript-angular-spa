3c35000671781d884cb9d0b6bdaf9246bfbd2b17
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-07-16 10:55:27 +1000

    format code

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -9,8 +9,8 @@ export const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.n
  * 2) Web API configuration parameters
  * 3) Authentication configuration parameters
  * 4) MSAL-Angular specific configuration parameters
- * ================================================= 
-*/ 
+ * =================================================
+*/
 
 // #region 1) B2C policies and user flows
 /**
@@ -19,66 +19,66 @@ export const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.n
  * To learn more about custom policies, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
  */
 export const b2cPolicies = {
-    names: {
-        signUpSignIn: "b2c_1_susi",
-        resetPassword: "b2c_1_reset",
+  names: {
+    signUpSignIn: "b2c_1_susi",
+    resetPassword: "b2c_1_reset",
+  },
+  authorities: {
+    signUpSignIn: {
+      authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_susi"
     },
-    authorities: {
-        signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_susi"
-        },
-        resetPassword: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_reset"
-        } 
+    resetPassword: {
+      authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_reset"
     }
+  }
 }
 // #endregion
 
 
 // #region 2) Web API Configuration
-/** 
+/**
  * Enter here the coordinates of your Web API and scopes for access token request
  * The current application coordinates were pre-registered in a B2C tenant.
  */
-export const apiConfig: {b2cScopes: string[], webApi: string} = {
-    b2cScopes: ['https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read'],
-    webApi: 'https://fabrikamb2chello.azurewebsites.net/hello'
+export const apiConfig: { b2cScopes: string[], webApi: string } = {
+  b2cScopes: ['https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read'],
+  webApi: 'https://fabrikamb2chello.azurewebsites.net/hello'
 };
 // #endregion
 
 
 
 // #region 3) Authentication Configuration
-/** 
+/**
  * Config object to be passed to Msal on creation. For a full list of msal.js configuration parameters,
  * visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_configuration_.html
  */
 export const msalConfig: Configuration = {
-    auth: {
-        clientId: "e760cab2-b9a1-4c0d-86fb-ff7084abd902",
-        authority: b2cPolicies.authorities.signUpSignIn.authority,
-        redirectUri: "http://localhost:6420/",
-        postLogoutRedirectUri: "http://localhost:6420/",
-        navigateToLoginRequestUrl: true,
-        validateAuthority: false,
-      },
-    cache: {
-        cacheLocation: "localStorage",
-        storeAuthStateInCookie: isIE, // Set this to "true" to save cache in cookies to address trusted zones limitations in IE
-    },
+  auth: {
+    clientId: "e760cab2-b9a1-4c0d-86fb-ff7084abd902",
+    authority: b2cPolicies.authorities.signUpSignIn.authority,
+    redirectUri: "http://localhost:6420/",
+    postLogoutRedirectUri: "http://localhost:6420/",
+    navigateToLoginRequestUrl: true,
+    validateAuthority: false,
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: isIE, // Set this to "true" to save cache in cookies to address trusted zones limitations in IE
+  },
 }
 
-/** 
- * Scopes you enter here will be consented once you authenticate. For a full list of available authentication parameters, 
+/**
+ * Scopes you enter here will be consented once you authenticate. For a full list of available authentication parameters,
  * visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_authenticationparameters_.html
  */
-export const loginRequest: {scopes: string[]} = {
-    scopes: ['openid', 'profile'],
+export const loginRequest: { scopes: string[] } = {
+  scopes: ['openid', 'profile'],
 };
 
 // Scopes you enter will be used for the access token request for your web API
-export const tokenRequest: {scopes: string[]} = {
-    scopes: apiConfig.b2cScopes // i.e. [https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read]
+export const tokenRequest: { scopes: string[] } = {
+  scopes: apiConfig.b2cScopes // i.e. [https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read]
 };
 // #endregion
 
@@ -87,21 +87,21 @@ export const tokenRequest: {scopes: string[]} = {
 // #region 4) MSAL-Angular Configuration
 // here you can define the coordinates and required permissions for your protected resources
 export const protectedResourceMap: [string, string[]][] = [
-    [apiConfig.webApi, apiConfig.b2cScopes] // i.e. [https://fabrikamb2chello.azurewebsites.net/hello, ['https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read']]
+  [apiConfig.webApi, apiConfig.b2cScopes] // i.e. [https://fabrikamb2chello.azurewebsites.net/hello, ['https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read']]
 ];
 
-/** 
+/**
  * MSAL-Angular specific authentication parameters. For a full list of available options,
  * visit https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/lib/msal-angular#config-options-for-msal-initialization
 */
 export const msalAngularConfig: MsalAngularConfiguration = {
-    popUp: !isIE,
-    consentScopes: [
-        ...loginRequest.scopes,
-        ...tokenRequest.scopes,
-    ],
-    unprotectedResources: [], // API calls to these coordinates will NOT activate MSALGuard
-    protectedResourceMap,     // API calls to these coordinates will activate MSALGuard
-    extraQueryParameters: {}  
-}
+  popUp: !isIE,
+  consentScopes: [
+    ...loginRequest.scopes,
+    ...tokenRequest.scopes,
+  ],
+  unprotectedResources: [], // API calls to these coordinates will NOT activate MSALGuard
+  protectedResourceMap,     // API calls to these coordinates will activate MSALGuard
+  extraQueryParameters: {}
+};
 // #endregion
